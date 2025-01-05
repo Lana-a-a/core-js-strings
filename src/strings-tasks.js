6c1b259a4945c8fe30a593da8e79186dd1e7df34
Lana Aivazovi <lana.aivazovi@gmail.com>
2025-01-06 03:39:34 +0400

    fix: fix code

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -300,8 +300,11 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ?
+   matches.length :
+    0;
 }
 
 /**
